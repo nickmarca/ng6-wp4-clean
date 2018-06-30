14a378102936381a1001d235b6f4ae150b3fc852
Nicholas Marcaccini <nicholasmarca@gmail.com>
2018-06-30 13:25:05 +0100

    added npm dev script; added webpack plugins;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: 'Hello World ! my-app  !!!! eita  it worked !!!!!! fuuuuck'
+    template: `
+      <div>
+        <h1>HELLO WORLD !</h1>
+        
+        <p>This project is a clean Angular starter template who I plan to maintain as far as I use Angular at my Job. </p>
+        <p>The reason why I decided to create this is because I want to have a clean, not opinated and light alternative to Angular Cli. </p>
+        <p>This is not suppose to do all the things Angular CLI does but it is suppose to do the *main* job Angular CLI does that is transpile Angular Typescript code.</p>
+      </div>
+    `
 })
 export class AppComponent { }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const {AngularCompilerPlugin} = require('@ngtools/webpack');
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWepackPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
     entry: path.resolve("./src/main.ts"),
@@ -27,6 +30,15 @@ module.exports = {
             tsConfigPath: path.resolve('./tsconfig.json'),
             entryModule: path.resolve('./src/app/app.module#AppModule'),
             sourceMap: true
-        })
+        }),
+
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+
+        new CopyWepackPlugin([
+          path.resolve(__dirname, 'node_modules', 'core-js', 'client', 'shim.js'),
+          path.resolve(__dirname, 'node_modules', 'zone.js', 'dist', 'zone.js')
+        ])
     ]
-}
+};
